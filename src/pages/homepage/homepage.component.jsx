808c29f3d978482a8559ddc08d8d92a02d83fb3d
olga.goncharuk <olga.goncharuk@ibsscorp.com>
2020-12-16 21:44:27 -0500

    broken out homepage into smaller components, finished styling our menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomaPage = () => (
+    <div className = "homepage">
+        <Directory/>
+    </div>
+);
+
+export default HomaPage;
